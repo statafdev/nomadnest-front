55634497b129833c9e4c4ca5d5ccdf91e44dbbac
zakezakikou <zakezakikou@gmail.com>
2025-11-24 15:30:23 +0100

    my-listing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "example.com",
+      "localhost",
+      "fq3on1usp0ruto0i.public.blob.vercel-storage.com",
+    ], // ajoute ici les domaines de tes images
+  },
 };
 
 export default nextConfig;
